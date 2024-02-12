44ab5f537b86da6fd8adefcfad899e2dcf1cbd26
Muhammad Zeshan <156847467+Zeshan0074@users.noreply.github.com>
2024-02-13 00:32:39 +0500

    work on top bar and sidebar continue

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+// import React from 'react'
 
-export default function Sidebar() {
+// export default function Sidebar() {
+//   return (
+//     <div>Sidebar</div>
+//   )
+// }
+
+
+import { Sidebar as ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+
+function Sidebar() {
   return (
-    <div>Sidebar</div>
-  )
+    <ProSidebar>
+      <Menu iconShape="square">
+        <SubMenu title="Charts">
+          <MenuItem> Pie charts </MenuItem>
+          <MenuItem> Line charts </MenuItem>
+        </SubMenu>
+        <MenuItem> Documentation </MenuItem>
+        <MenuItem> Calendar </MenuItem>
+      </Menu>
+    </ProSidebar>
+  );
 }
+
+export default Sidebar;
--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,7 +1,57 @@
-import React from 'react'
+import React from "react";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import { Box, IconButton, InputBase } from "@mui/material";
+import {
+  LightModeOutlined as LightModeIcon,
+  DarkModeOutlined as DarkModeIcon,
+  NotificationsActiveOutlined as NotificationsIcon,
+  SettingsOutlined as SettingsIcon,
+  PersonOutlined as PersonIcon,
+  SearchOutlined as SearchIcon,
+} from "@mui/icons-material";
+import { useTheme } from "@emotion/react";
 
 export default function Topbar() {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
   return (
-    <div>Topbar</div>
-  )
+    //This is the outer box of the top bar
+    <Box display="flex" justifyContent="space-between" p={2}>
+      {/* This box is for Searchbar and it's icon */}
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchIcon />
+        </IconButton>
+      </Box>
+
+      {/* This box is for other icon on the right side */}
+
+      <Box display="flex" justifyContent="space-between">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? <DarkModeIcon /> : <LightModeIcon />}
+         
+        </IconButton>
+
+        <IconButton>
+          <NotificationsIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsIcon />
+        </IconButton>
+        <IconButton>
+          <PersonIcon />
+        </IconButton>
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+      </Box>
+    </Box>
+  );
 }

6906b5add69799e32d1ab16f64c06421bde72e61
Muhammad Zeshan <156847467+Zeshan0074@users.noreply.github.com>
2024-02-12 22:52:07 +0500

    add node modules and previous code

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
+function App() {
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app"></div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+export default App;
